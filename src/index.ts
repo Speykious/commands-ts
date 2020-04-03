eada4114b8532846e439328f4565d5fff8222194
Speykious <speykious.the.king@gmail.com>
2020-04-03 17:30:35 +0200

    yay

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import 'parsers-ts';
+import { word, spaces, str } from 'parsers-ts/maps/ParserCreators';
+import { colors } from './colors';
+
+//prefix:commands <arg1> <arg2> <arg3> [arg4] [arg5] [arg6]
