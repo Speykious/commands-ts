64fd7e7d24c8680625bc344b5974e497750fc425
Speykious <speykious.the.king@gmail.com>
2020-04-22 13:31:47 +0200

    Proper tests

--- tests/options.test.ts
+++ tests/options.test.ts
@@ -1,7 +1,7 @@
 import { Option } from '../src/Option'
 import { defaultTypes } from '../src/defaultTypes'
 
-test('Options: simple - professeur', () => {
+test('Options: without arguments - professeur', () => {
 	const opProf = new Option(defaultTypes, {
 		name: 'professeur',
 		description: `Si l'utilisateur est un professeur.`,
@@ -23,4 +23,50 @@ test('Options: simple - professeur', () => {
 	expect(short1.result).toBe(true)
 	expect(short2.result).toBe(true)
 	expect(short3.result).toBe(true)
+})
+
+test('Options: with arguments - answer', () => {
+	const opProf = new Option(defaultTypes, {
+		name: 'answer',
+		description: `To answer yes, or no.`,
+		short: 'a',
+		arguments: [
+			{
+				label: 'answer',
+				description: 'The answer: yes, or no.',
+				type: 'word',
+				oneOf: ['yes', 'no']
+			}
+		]
+	})
+
+	const long1 = opProf.parse("--answer")
+	const long2 = opProf.parse("--answer ...then waste of string")
+	const long3 = opProf.parse("--answer yes")
+	const long4 = opProf.parse("--ansnbhvf")
+
+	expect(long1.error.info).toBe('Argument n°1 from option "answer" is invalid')
+	expect(long2.error.info).toBe('Argument n°1 from option "answer" is invalid')
+	expect(long3.error).toBe(null)
+	expect(long4.error.info).toBe('Option "answer" parsing failed')
+
+	expect(long1.result).toBe(null)
+	expect(long2.result).toBe(null)
+	expect(long3.result).toEqual(['yes'])
+	expect(long4.result).toBe(null)
+
+	const short1 = opProf.parse("-a")
+	const short2 = opProf.parse("-a ...then waste of string")
+	const short3 = opProf.parse("-a yes")
+	const short4 = opProf.parse("-ahjivf")
+
+	expect(short1.error.info).toBe('Argument n°1 from option "answer" is invalid')
+	expect(short2.error.info).toBe('Argument n°1 from option "answer" is invalid')
+	expect(short3.error).toBe(null)
+	expect(short4.error.info).toBe('Argument n°1 from option "answer" is invalid')
+
+	expect(short1.result).toBe(null)
+	expect(short2.result).toBe(null)
+	expect(short3.result).toEqual(['yes'])
+	expect(short4.result).toBe(null)
 })
